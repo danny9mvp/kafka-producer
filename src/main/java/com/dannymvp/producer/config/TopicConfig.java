b9d6724062ea4cd1518d261817e55884a66e586a
danny9mvp <danny9mvp@gmail.com>
2026-01-24 10:01:51 -0500

    feature: topic config

---
+++ src/main/java/com/dannymvp/producer/config/TopicConfig.java
@@ -0,0 +1,27 @@
+package com.dannymvp.producer.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Map;
+
+@Configuration
+public class TopicConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress));
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return new NewTopic("danny-topic", 1, (short) 1);
+    }
+}
