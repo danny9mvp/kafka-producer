956ad68f12ce9e15e3803beff870611e4880d8ff
danny9mvp <danny9mvp@gmail.com>
2026-01-24 09:00:29 -0500

    feature: first commit

---
+++ src/test/java/com/dannymvp/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannymvp.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
